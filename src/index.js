56d7fbef29e51159cb5fc16066a2abbb799b276d
saliamanovich <saliamanovicha@gmail.com>
2022-01-07 19:25:38 +0300

    complete function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let massiv = n.toString().split('');
+    let massiv_reverse = n.toString().split('').reverse();
+    let end_simbol = massiv_reverse.length-1;
+    let last_simbol = massiv_reverse[end_simbol];
+    
+    
+    if (Number.isInteger(+last_simbol)) {
+        return massiv_reverse.join('');
+    }
+    else {
+        let result = massiv.splice(0, 1);
+        return massiv.reverse().join('');
+    }
+    
+    
+    
+    
 }
